bcf2b017db5b8186a48b1b20d2bb653b59eef581
Ugnius <ugnius.malukas@gmail.com>
2020-08-22 18:12:52 +0900

    Added player.

---
+++ data_player/player.py
@@ -0,0 +1,77 @@
+import argparse
+import cv2
+import numpy as np
+import scipy.io as sio
+import utils
+
+
+def display_window(video_file_path, points):
+    cap = cv2.VideoCapture(video_file_path)
+    current_frame_num = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret:
+            frame_points = points[current_frame_num]
+            for idx, val in enumerate(frame_points):
+                frame = cv2.circle(
+                    frame,
+                    (val[0], val[1]),
+                    radius=2,
+                    color=(124, 252, 0),
+                    lineType=cv2.LINE_AA,
+                    thickness=-1
+                )
+
+            # Display the resulting frame
+            cv2.imshow('Motion Capture', frame)
+
+            current_frame_num = current_frame_num + 1
+
+            # Close program by using key
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def run_player(extrinsic_data_path, camera_data_path, motion_capture_data_path, video_file_path):
+    extrinsic_data = np.load(extrinsic_data_path, )
+    rotation_matrix = extrinsic_data['rotationMatrix']
+    translation_vector = extrinsic_data['translationVector']
+
+    camera_data = np.load(camera_data_path)
+    intrinsic_matrix = camera_data['IntrinsicMatrix']
+
+    motion_capture_data = \
+        sio.loadmat(motion_capture_data_path, simplify_cells=True)['Subject_1_F_amass']['move']
+    joints = motion_capture_data[0]['jointsLocation_amass']
+
+    points = utils.adapt_motion_data_for_video(joints, rotation_matrix, translation_vector, intrinsic_matrix)
+    display_window(video_file_path, points)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--extrinsic_data',
+                        help='Path to the camera\'s extrinsic parameters.',
+                        default='../data/Calib/Extrinsics_PG1.npz',
+                        type=str)
+    parser.add_argument('--camera_data',
+                        help='Path to the camera\'s parameters file.',
+                        default='../data/Calib/cameraParams_PG1.npz',
+                        type=str)
+    parser.add_argument('--motion_capture_data',
+                        help='Path to the motion capture file.',
+                        default='../data/AMASS/F_amass_Subject_1.mat',
+                        type=str)
+    parser.add_argument('--video_file',
+                        help='Path to the video file.',
+                        default='../data/PG1/F_PG1_Subject_1_L1.avi',
+                        type=str)
+    args = parser.parse_args()
+    run_player(args.extrinsic_data, args.camera_data, args.motion_capture_data, args.video_file)
+
