5caca8804edba6fe5eae7ab6e5537df6533f06d3
Ugnius <ugnius.malukas@gmail.com>
2020-08-23 11:33:13 +0900

    Added Camera dataclass.

---
+++ data_player/camera.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class Camera:
+    """Camera's parameters"
+    :param rotation_matrix: camera's rotation matrix
+    :type rotation_matrix: np.ndarray
+    :param translation_vector: camera's translation vector
+    :type translation_vector: np.ndarray
+    :param intrinsic_matrix: camera's intrinsic matrix
+    :type intrinsic_matrix: np.ndarray
+    """
+    rotation_matrix: np.ndarray
+    translation_vector: np.ndarray
+    intrinsic_matrix: np.ndarray
