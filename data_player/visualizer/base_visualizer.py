6a5ef7843cca9adeceab65e1adffda21b5545fb1
Ugnius <ugnius.malukas@gmail.com>
2020-09-21 12:22:35 +0900

    Updated the BaseVisualizer class.

--- data_player/visualizer/base_visualizer.py
+++ data_player/visualizer/base_visualizer.py
@@ -1,12 +1,28 @@
-# Python 3.4+
 from abc import ABC, abstractmethod
 
 
 class BaseVisualizer(ABC):
+    """
+    Base class for visualizers.
+    """
     @abstractmethod
     def update(self, frame):
+        """
+        Update the animation of the matplotlib figure.
+
+        :param frame: frame number
+        :type frame: int
+        """
         pass
 
     @abstractmethod
     def get_animation(self, fps=30):
+        """
+        Get animation.
+
+        :param fps: frames per second
+        :type fps: int
+        :return: matplotlib animation
+        :rtype: matplotlib.animation.FuncAnimation
+        """
         pass
